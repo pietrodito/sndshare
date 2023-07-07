2da59309848f4a41ab5a39acf96c7f02f5e8c99a
pietrodito <pietrodito@gmail.com>
2023-07-07 18:29:26 +0200

    Add xxd test to dir_to_txt()

--- tests/testthat/test-dir_to_txt.R
+++ tests/testthat/test-dir_to_txt.R
@@ -32,9 +32,9 @@ test_that("error if wrong encoding scheme", {
                "scheme")
 })
 
-test_that("encoding has worked", {
+test_that("base64 encoding works", {
 
-  tmp_dir <- glue::glue(".{testthat::test_path(tempdir())}")
+  tmp_dir <- glue::glue("./{testthat::test_path(tempdir())}")
   fs::dir_create(tmp_dir)
 
   dir_to_zip <- glue::glue("{tmp_dir}/dir_to_zip")
@@ -46,7 +46,28 @@ test_that("encoding has worked", {
   dir_to_txt(dir_path = dir_to_zip,
              txt_file_path = txt_file)
 
-  expected_size <- 7020L
+  expected_size <- 7093L
+  actual_size <- fs::file_size(txt_file) %>% as.integer()
+
+  expect_equal(actual_size, expected_size)
+})
+
+test_that("xxd encoding works", {
+
+  tmp_dir <- glue::glue("./{testthat::test_path(tempdir())}")
+  fs::dir_create(tmp_dir)
+
+  dir_to_zip <- glue::glue("{tmp_dir}/dir_to_zip")
+  txt_file   <- testthat::test_path("txt_file.txt")
+  withr::defer(fs::file_delete(txt_file))
+
+  local_create_files(dir_to_zip, filenames = letters)
+
+  dir_to_txt(dir_path = dir_to_zip,
+             txt_file_path = txt_file,
+             encoding_scheme = "xxd")
+
+  expected_size <- 21976L
   actual_size <- fs::file_size(txt_file) %>% as.integer()
 
   expect_equal(actual_size, expected_size)
